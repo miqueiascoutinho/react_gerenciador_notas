d7065b3f42df855ca17194e1bb23f080c87083f9
Miqueias Coutinho <miqueias.sc@hotmail.com>
2021-06-04 14:24:42 -0300

    React: Adicionando Categorias

--- src/App.js
+++ src/App.js
@@ -1,43 +1,61 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./compoments/ListaDeNotas";
 import FormularioCadastro from "./compoments/FormularioCadastro";
+import ListaDeCategorias from "./compoments/ListaDeCategorias";
 import "./assets/App.css";
-import './assets/index.css';
+import "./assets/index.css";
 
 class App extends Component {
-
   constructor() {
-    super();    
+    super();
     this.state = {
-      notas: []
-    }
+      notas: [],
+      categorias: []
+    };
   }
 
   criarNota(titulo, descricao) {
-    const novaNota = {titulo, descricao};
+    const novaNota = { titulo, descricao };
     const todasNotas = [...this.state.notas, novaNota];
 
     this.setState({
-      notas:todasNotas
-    })
+      notas: todasNotas, 
+      categorias: this.state.categorias
+    });
   }
 
   excluirNota(indice) {
     this.notas = this.state.notas;
-    this.notas.splice(indice,1);
-  
+    this.notas.splice(indice, 1);
+
     this.setState({
       notas: this.notas
-    })
-    
+    });
+  }
 
+  criarCategoria(categoria) {
+    console.log(categoria);
+
+    this.setState({
+      notas: this.state.notas, 
+      categorias: [...this.state.categorias, categoria]
+    });
   }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro novaNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas notas={this.state.notas} excluirNota={this.excluirNota.bind(this)}/>
+        <FormularioCadastro novaNota={this.criarNota.bind(this)} />
+        <main className="conteudo-principal">
+          <ListaDeCategorias
+            criarCategoria={this.criarCategoria.bind(this)}
+            categorias={this.state.categorias}
+          />
+          <ListaDeNotas
+            notas={this.state.notas}
+            excluirNota={this.excluirNota.bind(this)}
+          />
+        </main>
       </section>
     );
   }
---
+++ src/compoments/ListaDeCategorias/ListaDeCategorias.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import "./estilo.css";
+class ListaDeCategorias extends Component {
+  criarCategoria(event) {
+    if (event.keyCode === 13) {
+        this.props.criarCategoria(event.target.value);
+        event.target.value = "";
+    }
+  }
+  render() {
+    return (
+      <section className="lista-categorias">
+        <ul className="lista-categorias_lista">
+          {this.props.categorias.map((categoria, indice) => {
+            return (
+              <li key={indice} className="lista-categorias_item">
+                {categoria}
+              </li>
+            );
+          })}
+        </ul>
+        <input
+          type="text"
+          placeholder="Digite uma categoria"
+          className="lista-categorias_input"
+          onKeyUp={this.criarCategoria.bind(this)}
+        />
+      </section>
+    );
+  }
+}
+
+export default ListaDeCategorias;
